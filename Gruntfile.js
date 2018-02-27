3da059dbd0e7488b6afd0719cfad82c97b2281c1
brandon van dongen <brandonvdongen@hotmail.com>
2018-02-27 14:21:05 +0100

    prepared project with base template

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'src/index.html',     // 'destination': 'source'
+                }
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: false,
+                presets: ['env']
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ["js/*.js"],
+                        dest: 'babel/'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'babel/',
+                        src: ["js/*.js"],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: ['*.css'],
+                    dest: 'dist/css'
+                }]
+            }
+        },
+        clean: {
+            build: ['babel/'],
+            release: ['dist/']
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('build', [
+        'clean',
+        'babel',
+        'uglify',
+        'cssmin',
+        'htmlmin',
+        'clean:build'
+    ]);
+
+    grunt.registerTask('default', ['build']);
+}
+;
